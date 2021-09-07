6945959f1bb8ec6963d39dcdb278f9fa82768aeb
Vladislav Kaipetskiy <vlad@Vladislavs-MacBook-Pro.local>
2021-09-07 16:01:51 +0100

    Relax security rules for now

---
+++ src/main/java/com/aws/codestar/projecttemplates/controller/SecurityConfig.java
@@ -0,0 +1,19 @@
+package com.aws.codestar.projecttemplates.controller;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+
+@Configuration
+@EnableGlobalMethodSecurity(prePostEnabled = true)
+public class SecurityConfig extends WebSecurityConfigurerAdapter {
+    protected void configure(final HttpSecurity http) throws Exception {
+        http.antMatcher("/**")
+                .authorizeRequests()
+                .antMatchers("/").permitAll()
+                .anyRequest().authenticated()
+//                .and().oauth2Login() // <-- THIS WAS CHANGED
+        ;
+    }
+}
