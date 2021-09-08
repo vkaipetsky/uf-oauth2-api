093cbf8c34754b32e2a74f5f38aca18d772ae664
Vladislav Kaipetskiy <vlad@Vladislavs-MacBook-Pro.local>
2021-09-08 09:50:35 +0100

    Allow cross-origin requests to come in

--- src/main/java/com/aws/codestar/projecttemplates/controller/WebController.java
+++ src/main/java/com/aws/codestar/projecttemplates/controller/WebController.java
@@ -2,18 +2,21 @@ package com.aws.codestar.projecttemplates.controller;
 
 import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
 public class WebController {
 
+    @CrossOrigin(origins = "*")
     @RequestMapping("/")
     @ResponseBody
     public String home() {
         return "Welcome home!";
     }
 
+    @CrossOrigin(origins = "*")
     @PreAuthorize("hasAuthority('admins')")
     @RequestMapping("/restricted")
     @ResponseBody
